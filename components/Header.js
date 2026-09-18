369dc5c786d7f67c55744f17f15d7ae087fbd53c
Mohamad <montikpi@gmail.com>
2026-09-18 10:57:48 +0200

    started adding things

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function header() {
+
+    let headerELm = document.createElement("header")
+    headerELm.classList.add("header ")
+
+    headerELm.innerHTML = `
+    
+    `
+
+    return headerELm; 
+}
--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-// slå dig løs her... 
+
+
+const rootElm = document.querySelector("#root")
+
+function render () {
+    rootElm.innerHTML = "";
+
+    
+}
+
+function init(){
+    render()
+
+}
+
+init()
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
